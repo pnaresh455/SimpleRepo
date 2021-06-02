020bd82dac0832ecc8d505998a8e57e6c724c1e2
naresh.p <naresh.p@innoviti.com>
2021-06-02 18:49:02 +0530

    new test class

---
+++ src/com/java/Test.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List ll = new ArrayList();
+	}
+
+}
