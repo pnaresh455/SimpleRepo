4247288eec2c2398ea42840f8351d48600b99ed3
naresh.p <naresh.p@innoviti.com>
2021-06-02 13:56:31 +0530

    HelloWorld java file

---
+++ src/com/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = null;
+		String k;
+		System.out.println("Hello World");
+	}
+
+}
